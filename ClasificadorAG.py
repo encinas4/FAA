6f769d06b4246a369c24dae430f84c74643be3fa
pelofeo <urdiales.guillermo@gmail.com>
2018-12-12 11:49:32 -0800

    Cambios

--- ClasificadorAG.py
+++ ClasificadorAG.py
@@ -8,101 +8,69 @@ class ClasificadorAG():
   nepocas =0;
 
   def __init__(self, n, p):
-      self.nepocas = n
-      self.p=p
-      pass
+    self.nepocas = n
+    self.p=p
+    pass
 
   def procesamiento(self, dataset, clasificador):
-    l = dataset.numeroAtributos-1
-    poblacion = np.random.randint(2, size=(self.p, l))
-    fitness = []
-    roulette = []
-    for n in range(self.nepocas):
+    tam = len(dataset.nombreAtributos)
+    intAle = np.random.randint(low=1,high=tam, size=(self.p,1),dtype=np.uint64)
+    binarios = np.array([np.hstack((np.ones(n, dtype=np.uint64), np.zeros(tam - n - 1, dtype=np.uint64))) for n in intAle])
+    binariosAle = np.array(binarios)
+
+    map(lambda x: np.random.shuffle(x),binariosAle)
+
+    poblacion = self.fitnessPob(binariosAle,dataset,clasificador)
+    for i in range(p):
+      if max(poblacion[0][1]>0.95):
+        break
+
+      pobAux = self.seleccionProgenitores(poblacion)
+      pobAux = self.cruceUniformePob(pobAux)
+      pobAux = self.mutacionPob(pobAux)
+      pobAux = self.fitnessPob(pobAux,dataset,clasificador)
+      poblacion = self.seleccionSup(pobAux,poblacion)
 
 
-      if(fitness != [] and (max(fitness)>=0.9)):
-        return
-          
-      fitness = []
-      roulette = []
-      cruzados = []
-      siguienteGen = []
-      ran = 1
-      
-      for elemento in poblacion:
-        
-          dataset_elem = copy.copy(dataset)
-          indices = list(np.flatnonzero(elemento))
 
-          dataset_elem.datos = dataset.extraeDatosRelevantes(indices)
-          dataset_elem.diccionarios = dataset.diccionarioRelevante(indices)
-          dataset_elem.atribDiscretos = dataset.atribDiscretosRelevantes(indices)
-          dataset_elem.nombreAtributos = dataset.nombreAtributosRelevantes(indices)
-         # print("Los atributos seleccionados son: ",dataset_elem.nombreAtributos[:-1])
+  def fitnessPob(self,binarios,dataset,clasificador):
+    estrategia = EstrategiaParticionado.ValidacionSimple(porcentajeTrain=0.7)
+    tam = len(binarios)
+    #l=Parallel(n_jobs=-1)(delayed(unwrap_self_fit)(p) for p in  izip([self] * tam, binarios, [dataset] * tam,[clasificador] * tam, [estrategia]*tam))
+    dataSetAux = Datos()
+    e=[]
+    c=[]
 
-          particionado = EstrategiaParticionado.ValidacionCruzada(10)
-          fitness.extend(clasificador.validacion(particionado, dataset_elem, clasificador))
-#Acierta siempre al clasificar con reg logistica lo cual es muy raro....
-          #print"El fitness es: ", 1-fitness[-1]
-          #print  fitness
-      fitness = list(1-np.array(fitness))
+    for c in binario:
+      colNum = np.flatnonzero(binarios)
+      dataSetAux.datos = dataset.extraeDatosRelevantes(colNum)
+      dataSetAux.diccionarios = dataset.diccionarioRelevante(colNum)
+      dataSetAux.nominalAtributos = dataset.atribDiscretosRelevantes(colNum)
+      e.extend(clasificador.validacion(estrategia, dataSetAux, clasificador))
+      c.extend(col)
+      #col y error que habria que ordenar?
+    return np.flatnonzero(poblacion[0][0]), poblacion[0][1]
+      
 
-      for i in range(len(fitness)):
-          norm = fitness[i]/sum(np.array(fitness))
-          roulette.append((poblacion[i], ran-norm, norm))
-          ran -= norm
-      
-      for i in range(int(self.p*0.6)):
-          random = np.random.rand()
-          for i in range(len(roulette)):
-              if ((random > roulette[i][1]) and (random < roulette[i-1][1])):
-                  cruzados.append(roulette[i][0])
-                  
-      for IndiceACruzar in range(1, len(cruzados), 2):
-          hijoUno = []
-          hijoDos = []
-          for subIndice in range(len(cruzados[IndiceACruzar])):
-              if (np.random.randint(2) == 0):
-                  hijoUno.append(cruzados[IndiceACruzar][subIndice])
-                  hijoDos.append(cruzados[IndiceACruzar - 1][subIndice])
-              else:
-                  hijoUno.append(cruzados[IndiceACruzar - 1][subIndice])
-                  hijoDos.append(cruzados[IndiceACruzar][subIndice])
-                  
-          siguienteGen.append(hijoUno)
-          siguienteGen.append(hijoDos)
-      
-      for i in list(sorted(roulette, key=lambda max:max[2], reverse=True)[0:int(self.p*0.05)]):
-          siguienteGen.append(list(i[0]))
-      print(siguienteGen)
-      
-      datosAMutar = []
-      numDatosMutacion = int(0.35*len(poblacion))
-      
-      for i in range(numDatosMutacion):
-          random = np.random.randint(len(poblacion))
-          datosAMutar.append(poblacion[random])
-      
-      for valor in datosAMutar:
-          for bit in range(len(valor)):
-              random = np.random.randint(1000)
-              if random < 1:
-                  if valor[bit] == 0: 
-                      valor[bit] = 1
-                  else: 
-                      valor[bit] = 0
-                                   
-      siguienteGen += datosAMutar
-      poblacion=np.array(siguienteGen)
-      
-      print"El maximo de fitness es: ",max(fitness)
-      tuple = list(sorted(roulette, key=lambda max:max[2], reverse=True))[0]
-      print "Max fitness:", tuple[0]
-      print "Ha seleccionado los siguientes atributos: "
-      
-      valoresSalida = []
-      
-      for i in range(len(tuple[0])):
-          if (tuple[0][i] == 1):
-              valoresSalida.append(dataset.nombreAtributos[i])
-      print valoresSalida
+
+    return sorted(l,key=lambda t: t[1], reverse=True)
+
+
+    def cruceUniformePob(self, pob):
+      for pp in pob:
+        for i in xrange(len(p)):
+            if random.random() < 0.6:
+                pp[0:2][i], pp[1:2][i] = pp[1:2][i], pp[0:2][i]
+      return pob
+    def mutacionPob(self, pob):
+      for p in pob:
+        for i in range(len(p)):
+            if random.random() < 0.001:
+                p[i] = 0 if p[i]==1 else 1
+
+    def seleccionSup(self,pobAux,poblacion):
+      aux = []
+      tamElite = int(math.ceil(0.05 * self.p))
+      aux.extend(poblacion[0:tamElite])
+      aux.extend(pobAux[0:(self.tamPob-tamElite)])
+      return sorted(aux,key=lambda t: t[1], reverse=True)
